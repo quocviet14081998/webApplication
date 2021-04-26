785c4cfb93dc21a8388fbface4a677ee66c2ae90
quocviet14081998 <quocviet14081998@gmail.com>
2021-04-26 12:07:15 +0700

    update

---
+++ JavaScript/bt05/js05.js
@@ -0,0 +1,10 @@
+function xoayChu() {
+    var str1 = document.getElementById("p1").innerHTML;
+    str1 = str1.substr(str1.length-1) +str1.substr(0, str1.length-1);
+    document.getElementById("p1").innerHTML = str1;
+    var str2 = document.getElementById("p2").innerHTML;
+    str2 = str2.substr(1) +str1.substr(0,1);
+    document.getElementById("p2").innerHTML = str2;
+}
+function xoayTrai() {
+}
