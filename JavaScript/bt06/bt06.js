2f18371a087b81ff0ad4c0c39d71e884e208825a
quocviet14081998 <quocviet14081998@gmail.com>
2022-09-30 14:37:53 +0700

    update

--- JavaScript/bt06/bt06.js
+++ JavaScript/bt06/bt06.js
@@ -1,16 +1,16 @@
 var html = "<table><caption>BẢNG CỬU CHƯƠNG</caption>";
-        var count = 2,
-            row, col, i;
-        for (row = 1 ; row<= 2 ; row++) {
-            html += "<tr>";
-            for(col = 1; col <= 4 ; col++){
-                html += "<td>";
-                for(i = 1 ; i <= 9 ; i++)
-                    html += count + " x " + i + " = " + count * i + "<br>";
-                count++;
-                html += "</td>";
-            }
-            html += "</tr>";
-        }
-        html += "</table>";
-        document.write(html);
+var count = 2,
+row, col, i;
+for (row = 1 ; row<= 2 ; row++) {
+    html += "<tr>";
+    for(col = 1; col <= 4 ; col++){
+        html += "<td>";
+        for(i = 1 ; i <= 9 ; i++)
+            html += count + " x " + i + " = " + count * i + "<br>";
+        count++;
+        html += "</td>";
+    }
+    html += "</tr>";
+}
+html += "</table>";
+document.write(html);
